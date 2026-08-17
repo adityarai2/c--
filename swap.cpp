c7618ae911983b9fecbc923b818d36c63cb21ce9
Aditya rai <149267963+adityarai2@users.noreply.github.com>
2026-08-17 22:01:03 +0530

    some basics

--- swap.cpp
+++ swap.cpp
@@ -1,5 +1,10 @@
 #include<iostream>
 using namespace std;
 int main(){
+    int a=10,b=5,temp;
+    swap(a, b); 
+
+    cout << "a = " << a << ", b = " << b << endl; // Prints: a = 10, b = 5
+    return 0;
     
 }
