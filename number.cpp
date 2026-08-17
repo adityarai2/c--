517eb1d87ce688562a9499c3f185f3ff4046d1b0
Aditya rai <149267963+adityarai2@users.noreply.github.com>
2026-08-17 22:07:49 +0530

    basics3

---
+++ number.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[] ={10,20,30,40,50};
+    int n = sizeof(arr)/
+    sizeof(arr[0]);
+    cout<<"number of elements in this array ="<<n;
+    return 0;
+}
